40d0b2b3582d823cfd3c09d5fbbcc4d3c1c02b9e
Stevenson Marquez <stevensonxmarquez@gmail.com>
2019-02-03 10:08:19 -0500

    Cors Header

--- server/config/express.js
+++ server/config/express.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const compression = require("compression");
 const bodyParser = require("body-parser");
 const helmet = require("helmet");
+const cors = require("cors");
 const env = require("./env");
 const swagger = require("./swagger");
 
@@ -12,6 +13,7 @@ function config(app) {
   app.use(env.prod ? compression() : morgan("dev"));
   app.use(bodyParser.json());
   app.use(helmet());
+  app.use(cors());
   swagger(app);
 }
 
